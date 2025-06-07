361aac532d810b117c9d4f79f4db662c0058d255
ragasudha21 <ragasudhamadhiraju@gmail.com>
2025-06-07 13:48:00 +0530

    Add files via upload

---
+++ plot_test.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+
+plt.plot(x, y)
+plt.title("Simple Line Graph")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.grid(True)
+plt.show()
